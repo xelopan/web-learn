38cda47b195e400a8f8ecbb379bb74c6b5d2aaa6
Tri S. Abimanyu <tri-siswandhono.abimanyu@viseo.com>
2019-12-20 17:17:18 +0700

    Added sass

--- server/main.js
+++ server/main.js
@@ -4,9 +4,10 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
-app.use(express.static('server/public'));
+app.use(express.static('client/public'));
 
 app.get('/', (req, res) => res.send('Hello Word!'));
-app.get('/conquer', (req, res) => res.sendFile(path.join(__dirname, '/public/conquer.html')));
+app.get('/conquer', (req, res) => res.sendFile(path.join(__dirname, '../client/public/conquer.html')));
+app.get('/mypage', (req, res) => res.sendFile(path.join(__dirname, '../client/page.html')));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
